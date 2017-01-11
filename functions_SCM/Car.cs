6dc291a25f6734c80c44ce26fdde81ebda250e35
Shaun Milby <smilby0247@my.sctd.edu>
2017-01-10 21:09:25 -0500

    Add project files.

---
+++ functions_SCM/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functions_SCM
+{
+    class Car
+    {
+        public string Color {
+            get {
+                
+
+            }
+
+            set
+            {
+                
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+        public Car(string color)
+        {
+            this.Color = color;
+        }
+
+        public string Describe()
+        {
+            return "This car is " + this.Color;
+        }
+    }
+}
---
+++ functions_SCM/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functions_SCM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            cars.Add(new Car("Black"));
+            cars.Add(new Car("Purple"));
+            cars.Add(new Car("Red"));
+            cars.Add(new Car("Green"));
+            cars.Add(new Car("Primer Grey"));
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car.Describe());
+            }
+
+
+            Console.ReadKey();
+           
+
+
+        }
+    }
+}
+       
